27c88e13b752a275b958a0f6dfdf093e1c7ce5d8
cristianLupescu <cscristi11@gmail.com>
2020-06-29 11:50:32 +0300

    Solved Share

--- app/src/main/java/com/example/projectpanda/ShareActivity.java
+++ app/src/main/java/com/example/projectpanda/ShareActivity.java
@@ -1,14 +1,21 @@
 package com.example.projectpanda;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.FileProvider;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.StrictMode;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -18,6 +25,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 
 public class ShareActivity extends AppCompatActivity {
 
@@ -35,33 +43,41 @@ public class ShareActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 //get image as bitmap from ImageView
-                Drawable myDrawable = mred_pandaIV.getDrawable();
-                Bitmap bitmap = ((BitmapDrawable)myDrawable).getBitmap();
+               // Drawable myDrawable = mred_pandaIV.getDrawable();
+                //Bitmap bitmap = ((BitmapDrawable) myDrawable).getBitmap();
 
                 //sharing intent
 
+                if (checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(ShareActivity.this,
+                            new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                            1);
+                    return;
+                }
 
+                StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
+                StrictMode.setVmPolicy(builder.build());
+
+                Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.red_panda);
+                String path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES) + "/LatestShare.jpg";
+                OutputStream out = null;
+                File file = new File(path);
                 try {
-                    File file = new File(ShareActivity.this.getExternalCacheDir(),"red_panda.jpg");
-                    FileOutputStream fOut = new FileOutputStream(file);
-                    bitmap.compress(Bitmap.CompressFormat.PNG,80,fOut);
-                    fOut.flush();
-                    fOut.close();
-                    file.setReadable(true,false);
-                    //sharing intent
-                    Intent intent = new Intent(Intent.ACTION_SEND);
-                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                    intent.putExtra(Intent.EXTRA_STREAM, FileProvider.getUriForFile(getApplicationContext(),getPackageName()+".fileprovider",file));
-                    intent.setType("red_panda.jpg");
-                    startActivity(Intent.createChooser(intent,"Share Image Via"));
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
-                    Toast.makeText(ShareActivity.this,"File not found",Toast.LENGTH_SHORT).show();
-                }catch (IOException e){
-                    e.printStackTrace();
-                }catch (Exception e){
+                    out = new FileOutputStream(file);
+                    bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
+                    out.flush();
+                    out.close();
+                } catch (Exception e) {
                     e.printStackTrace();
                 }
+                path = file.getPath();
+                Uri bmpUri = Uri.parse("file://" + path);
+                Intent shareIntent = new Intent();
+                shareIntent = new Intent(android.content.Intent.ACTION_SEND);
+                shareIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                shareIntent.putExtra(Intent.EXTRA_STREAM, bmpUri);
+                shareIntent.setType("image/jpg");
+                startActivity(Intent.createChooser(shareIntent, "Share with"));
             }
         });
     }
